a63cbc5355a263d528757e1853c26437c7a9e837
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-09 14:59:33 +0300

    adding update image configurations'

--- clone/models.py
+++ clone/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='MEDIA/')
+    pass
+class Likes(models.Model):
+    pass
+class Comments(models.Model):
+    pass
+class Profile(models.Model):
+    pass
