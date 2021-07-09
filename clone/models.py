4e1111405d5e3ea1f0af45c5a1f8aa6a9b13ce2a
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-09 15:18:40 +0300

    adding profile and comment class

--- clone/models.py
+++ clone/models.py
@@ -4,11 +4,18 @@ from django.db import models
 class Likes(models.Model):
     name = models.CharField(max_length=30)
 
-    
+
 class Comments(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 class Profile(models.Model):
-    pass
+    bio = models.CharField(max_length=100)
+    Profile_photo= models.ImageField(upload_to='MEDIA/')
+    
+    def __str__(self):
+        return self.bio
+
 class Image(models.Model):
     name = models.CharField(max_length=30)
     image = models.ImageField(upload_to='MEDIA/')
