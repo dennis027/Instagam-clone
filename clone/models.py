5d3f0ff0fdbb99101304bfcbf90c3462fd085e13
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-09 15:07:23 +0300

    adding a like class and image class

--- clone/models.py
+++ clone/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    image = models.ImageField(upload_to='MEDIA/')
-    pass
 class Likes(models.Model):
-    pass
+    name = models.CharField(max_length=30)
+
+    
 class Comments(models.Model):
     pass
 class Profile(models.Model):
     pass
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='MEDIA/')
+    caption=models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.name
+
