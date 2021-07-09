bade2a99bb0a441a35885f864f50a378f57fe97b
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-09 23:57:32 +0300

    updating my classes in models.py

--- clone/models.py
+++ clone/models.py
@@ -10,6 +10,13 @@ class Likes(models.Model):
     name = models.CharField(max_length=30,primary_key=True)
     def __str__(self):
         return self.name
+    def save_like(self):
+        self.save()
+    def delete_like(self):
+        self.delete()
+    @classmethod
+    def update_like(cls,id):
+        cls.objects.filter(id=id).update(like=object)    
 
 class Comments(models.Model):
     comment = models.CharField(max_length=255,primary_key=True)
@@ -26,6 +33,14 @@ class Profile(models.Model):
     def __str__(self):
         return self.bio
 
+    def save_profile(self):
+        self.save()
+    def delete_profile(self):
+        self.delete()
+    @classmethod
+    def update_profile(cls,id):
+        cls.objects.filter(id=id).update(profile=object)    
+
 class Image(models.Model):
     name = models.CharField(max_length=30,primary_key=True)
     image = models.ImageField(upload_to='MEDIA/')
@@ -36,3 +51,10 @@ class Image(models.Model):
     def __str__(self):
         return self.name
 
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+    @classmethod
+    def update_image(cls,id):
+        cls.objects.filter(id=id).update(image=object)
